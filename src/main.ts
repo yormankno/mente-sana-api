e65380b086b88b927dca07d32c391a01bc80fdee
yormankno <yormankno@gmail.com>
2025-08-10 23:47:57 -0500

    cors de la api

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
